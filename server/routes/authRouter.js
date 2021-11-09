f9307b34324a35c5e4052ccd5d6cb588d576aa13
Aman <amsingh714@gmail.com>
2021-11-09 16:36:35 -0500

    able to update users password

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 // const db = require("../db/dbConfig");
 const upload = require("../util/multer");
+const restricted = require("../util/restricted");
 
 const {
   getAllUsers,
@@ -11,13 +12,15 @@ const {
   getUserInfo,
   updateUserInfo,
   deleteUserInfo,
+  updatePassword,
 } = require("../controllers/authControllers");
 
 router.get("/test", getAllUsers);
 router.post("/register", register);
 router.post("/login", login);
 router.get("/:unique_id", getUserInfo);
-router.put("/:unique_id", upload.single("profile_image"), updateUserInfo);
-router.delete("/:unique_id", deleteUserInfo);
+router.put("/:unique_id", restricted, upload.single("profile_image"), updateUserInfo);
+router.delete("/:unique_id", restricted, deleteUserInfo);
+router.put("/:unique_id/update_password", restricted, updatePassword);
 
 module.exports = router;
