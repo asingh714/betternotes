f561d0884d10723c3249f7a87fd2a2ea89fd261a
Aman <amsingh714@gmail.com>
2021-11-23 14:54:22 -0500

    able to delete a note with specific key

--- server/controllers/notesController.js
+++ server/controllers/notesController.js
@@ -63,19 +63,23 @@ const createNote = async (req, res) => {
       };
       // let savedProduct = {};
 
-      db("products").insert(newProduct).then(result => {
-        db("notes").insert(newNote).then(result => {
-                db("products")
+      db("products")
+        .insert(newProduct)
+        .then((result) => {
+          db("notes")
+            .insert(newNote)
+            .then((result) => {
+              db("products")
                 .join("notes", "products.note_key", "notes.note_key")
                 .select("*")
                 .orderBy("id", "desc")
                 .limit(1)
-                .then(result => {
+                .then((result) => {
                   console.log(result);
                   res.status(201).json(result);
-                })
-        })
-      })
+                });
+            });
+        });
     } else {
       res.status(500).json({
         message: "SOMETHING WENT WRONG",
@@ -110,8 +114,8 @@ const getSingleNote = (req, res) => {
     .select("*")
     .where({ "products.note_key": note_key })
     .then((singleNote) => {
-      console.log(singleNote)
-      if (singleNote.length < 1 ) {
+      console.log(singleNote);
+      if (singleNote.length < 1) {
         res.status(404).json({
           error: "You cannot access the note with this specific key",
         });
@@ -119,15 +123,124 @@ const getSingleNote = (req, res) => {
         res.status(200).json(singleNote);
       }
     })
-    .catch(error => {
+    .catch((error) => {
       res.status(500).json({
-        error: "The note with this specific ID could not be retrieved."
+        error: "The note with this specific ID could not be retrieved.",
+      });
+    });
+};
+
+const updateNote = (req, res) => {
+  const { note_key } = req.params;
+
+  const {
+    product_name,
+    short_description,
+    long_description,
+    document,
+    language,
+    school,
+    grade_level,
+    class_name,
+    teacher,
+  } = req.body;
+
+  const validationErrors = [];
+  if (
+    !product_name ||
+    !short_description ||
+    !long_description ||
+    !language ||
+    !school ||
+    !grade_level ||
+    !class_name ||
+    !teacher
+  ) {
+    validationErrors.push({
+      code: "VALIDATION_ERROR",
+      field: "ALL",
+      message: "Some of these fields are missing.",
+    });
+  }
+  console.log(validationErrors);
+  if (validationErrors.length) {
+    const errorObject = {
+      error: true,
+      errors: validationErrors,
+    };
+    res.status(400).send(errorObject);
+  } else {
+    db("products")
+      .where({ note_key })
+      .update({
+        product_name,
+        short_description,
+        long_description,
+        document,
+        language,
       })
+      .then((result) => {
+        db("notes")
+          .where({ note_key })
+          .update({
+            school,
+            grade_level,
+            class_name,
+            teacher,
+          })
+          .then((result) => {
+            db("products")
+              .join("notes", "products.note_key", "notes.note_key")
+              .select("*")
+              .orderBy("id", "desc")
+              .limit(1)
+              .then((result) => {
+                console.log(result);
+                res.status(201).json(result);
+              })
+              .catch((error) => {
+                res.status(500).json({
+                  error: "This note could not be retrieved.",
+                });
+              });
+          });
+      })
+      .catch((error) => {
+        res.status(500).json({
+          error: "This product could not be retrieved.",
+        });
+      });
+  }
+};
+
+const deleteNote = (req, res) => {
+  const { note_key } = req.params;
+
+  db("products")
+    .where({ note_key })
+    .first()
+    .then((product) => {
+      if (product) {
+        db("products").where({ note_key }).del();
+        db("notes")
+          .where({ note_key })
+          .del()
+          .then((count) => res.status(200).json(count));
+      } else {
+        res.status(404).json({
+          error: "You cannot access the product with this specific id.",
+        });
+      }
     })
+    .catch((error) => {
+      res.status(500).json({ error: "This product could not be removed" });
+    });
 };
 
 module.exports = {
   createNote,
   getAllNotes,
   getSingleNote,
+  updateNote,
+  deleteNote,
 };
--- server/routes/notesRouter.js
+++ server/routes/notesRouter.js
@@ -3,10 +3,23 @@ const router = express.Router();
 const upload = require("../util/multer");
 const authenticateUser = require("../util/authenticateUser");
 
-const { createNote, getAllNotes, getSingleNote } = require("../controllers/notesController");
+const {
+  createNote,
+  getAllNotes,
+  getSingleNote,
+  deleteNote,
+  updateNote,
+} = require("../controllers/notesController");
 
 router.get("/", getAllNotes);
-router.get("/:note_key", getSingleNote)
+router.get("/:note_key", getSingleNote);
 router.post("/", authenticateUser, upload.single("document"), createNote);
+router.put(
+  "/:note_key",
+  authenticateUser,
+  upload.single("document"),
+  updateNote
+);
+router.delete("/:note_key", authenticateUser, deleteNote);
 
 module.exports = router;
