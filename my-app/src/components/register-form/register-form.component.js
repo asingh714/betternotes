0322b3e3c038213fc620fd191633029efed1fd25
Aman <amsingh714@gmail.com>
2021-12-17 14:21:38 -0500

    completed register form with all inputs

--- my-app/src/components/register-form/register-form.component.js
+++ my-app/src/components/register-form/register-form.component.js
@@ -30,6 +30,34 @@ export default class RegisterForm extends Component {
           type="text"
           value={this.state.name}
         />
+        <FormInput
+          name="email"
+          handleChange={this.handleChange}
+          placeHolder="Email"
+          type="email"
+          value={this.state.email}
+        />
+        <FormInput
+          name="username"
+          handleChange={this.handleChange}
+          placeHolder="Username"
+          type="text"
+          value={this.state.username}
+        />
+        <FormInput
+          name="password"
+          handleChange={this.handleChange}
+          placeHolder="Password"
+          type="password"
+          value={this.state.password}
+        />
+        <FormInput
+          name="confirm_password"
+          handleChange={this.handleChange}
+          placeHolder="Confirm Password"
+          type="password"
+          value={this.state.confirm_password}
+        />
       </div>
     );
   }
