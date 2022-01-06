6201ebde55271ace8397a9052c048ef58d9c2307
Aman <amsingh714@gmail.com>
2022-01-06 13:21:51 -0500

    added node sass and basic styling for the entire project

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,9 +4,10 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+
+import "./globalStyles/index.scss";
 
 ReactDOM.render(
   <React.StrictMode>
