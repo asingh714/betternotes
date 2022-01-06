539a3a33290f021ce047b6442e278382b432fd0a
Aman <amsingh714@gmail.com>
2022-01-06 15:22:02 -0500

    styled the single note info containers on singlenote page

--- my-app/src/components/singleNote/singleNote.component.js
+++ my-app/src/components/singleNote/singleNote.component.js
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
+import image from "../../logo512.png";
+
+import Button from "../button/button.component";
+import "./singleNote.styles.scss";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
@@ -24,21 +28,37 @@ export default function SingleNote(props) {
   }
 
   return (
-    <div>
-      <Document
+    <div className="single-note-container">
+      {/* <Document
         file={document}
         loading="Loading IMAGE...."
         onLoadSuccess={onDocumentLoadSuccess}
+        className="noteImage"
       >
         <Page pageNumber={pageNumber} />
-      </Document>
-      <span>{class_name}</span>
-      <span>{short_description}</span>
-      <span>{price}</span>
-      <span>{user_name}</span>
-      <span>{pages}</span>
-      <span>{year}</span>
-      <span>{school}</span>
+      </Document> */}
+      <div className="img-container">
+        <img src={image} alt="" className="noteImage" />
+      </div>
+
+      <div className="note-info-container">
+        <div className="note-top-line">
+          <span>{class_name}</span>
+          <span>${price}</span>
+        </div>
+
+        <span>{short_description}</span>
+
+        <span>{user_name}</span>
+        <div className="note-bottom-line">
+          <div className="note-info-line">
+            <span>{pages}</span>
+            <span>{year}</span>
+            <span>{school}</span>
+          </div>
+          <Button type="submit">Add to Cart</Button>
+        </div>
+      </div>
     </div>
   );
 }
