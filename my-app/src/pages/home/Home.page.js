29554161131eba8f2994d843cc141f4b6f255c5f
Aman <amsingh714@gmail.com>
2022-01-30 12:54:34 -0500

    hero section styled

--- my-app/src/pages/home/Home.page.js
+++ my-app/src/pages/home/Home.page.js
@@ -1,5 +1,26 @@
-import React from "react";
+import "./Home.styles.scss";
+
+import Searchbar from "../../components/search-bar/SearchBar.component";
 
 export default function Home() {
-  return <div>HOME</div>;
+  return (
+    <div className="home-page-container">
+      <main className="hero-section">
+        <img
+          src="https://res.cloudinary.com/asingh/image/upload/v1643562491/brooke-cagle-JBwcenOuRCg-unsplash_suj1w1.jpg"
+          alt="blonde woman"
+          className="hero-image"
+        />
+        <div className="right-hero-section">
+          <h1>Where A's turn into cash. And B's and C's turn into A's.</h1>
+          <p>
+            Do you have great grades and take great notes? Sell your notes and
+            make money. Interested in improving your grades and saving time? Buy
+            notes from fellow students who have done the work for you.
+          </p>
+          <Searchbar inputStyle="long-search-input" />
+        </div>
+      </main>
+    </div>
+  );
 }
