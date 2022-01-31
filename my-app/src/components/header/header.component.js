f8b70b7fd5e813f7f11aadd7a0fed107ae6fea7c
Aman <amsingh714@gmail.com>
2022-01-31 13:34:39 -0500

    full content of header styled

--- my-app/src/components/header/header.component.js
+++ my-app/src/components/header/header.component.js
@@ -37,82 +37,84 @@ function Header({
 
   return (
     <header className="full-header-container">
-      <span onClick={() => navigate("/")} className="logo">
-        BETTER NOTE
-      </span>
-      <form action="submit" onSubmit={(e) => handleSearch(e)}>
-        <SearchBar
-          name={name}
-          handleChange={handleChange}
-          placeholder="&#x1F50D; Search by class subject"
-          type="text"
-          inputStyle="long-search-input"
-          value={value}
-        />
-      </form>
-      <nav className="right-header-container">
-        <span onClick={() => navigate("/notes")} className="nav-notes">
-          Notes
+      <div className="full-header-content-container">
+        <span onClick={() => navigate("/")} className="logo">
+          BETTER NOTE
         </span>
-        <IconContext.Provider
-          value={{
-            style: {
-              verticalAlign: "middle",
-              fontSize: "2em",
-              cursor: "pointer",
-              color: "#0552b5",
-            },
-          }}
-        >
-          <BsCart3 onClick={() => navigate("/cart")} />
-        </IconContext.Provider>
-        {isLoggedIn ? (
-          // <Button handleSubmit={handleLogout} buttonStyle="small-bluesix-btn">
-          //   Logout
-          // </Button>
-          <>
-            <img
-              src={profileData.profile_image}
-              alt="profile"
-              className="nav-profile-img"
-              onClick={() => setDropdown(!dropdown)}
-            />
-            {dropdown && (
-              <div className="nav-profile-dropdown">
-                <NavLink
-                  to="/profile"
-                  className="profile-nav-link"
-                  onClick={() => setDropdown(false)}
-                >
-                  Profile
-                </NavLink>
-                <NavLink
-                  onClick={handleLogout}
-                  to="/"
-                  // buttonStyle="small-bluesix-btn"
-                >
-                  Logout
-                </NavLink>
-              </div>
-            )}
-          </>
-        ) : (
-          <div className="nav-button-container">
-            <Button
-              handleSubmit={() => navigate("/login")}
-              buttonStyle="small-plain-btn"
-            >
-              Log In
-            </Button>
-            <Button
-              handleSubmit={() => navigate("/register")}
-              buttonStyle="small-bluesix-btn"
-            >
-              Sign Up
-            </Button>
-          </div>
-        )}
-      </nav>
+        <form action="submit" onSubmit={(e) => handleSearch(e)}>
+          <SearchBar
+            name={name}
+            handleChange={handleChange}
+            placeholder="&#x1F50D; Search by class subject"
+            type="text"
+            inputStyle="long-search-input"
+            value={value}
+          />
+        </form>
+        <nav className="right-header-container">
+          <span onClick={() => navigate("/notes")} className="nav-notes">
+            Notes
+          </span>
+          <IconContext.Provider
+            value={{
+              style: {
+                verticalAlign: "middle",
+                fontSize: "2em",
+                cursor: "pointer",
+                color: "#0552b5",
+              },
+            }}
+          >
+            <BsCart3 onClick={() => navigate("/cart")} />
+          </IconContext.Provider>
+          {isLoggedIn ? (
+            // <Button handleSubmit={handleLogout} buttonStyle="small-bluesix-btn">
+            //   Logout
+            // </Button>
+            <>
+              <img
+                src={profileData.profile_image}
+                alt="profile"
+                className="nav-profile-img"
+                onClick={() => setDropdown(!dropdown)}
+              />
+              {dropdown && (
+                <div className="nav-profile-dropdown">
+                  <NavLink
+                    to="/profile"
+                    className="profile-nav-link"
+                    onClick={() => setDropdown(false)}
+                  >
+                    Profile
+                  </NavLink>
+                  <NavLink
+                    onClick={handleLogout}
+                    to="/"
+                    // buttonStyle="small-bluesix-btn"
+                  >
+                    Logout
+                  </NavLink>
+                </div>
+              )}
+            </>
+          ) : (
+            <div className="nav-button-container">
+              <Button
+                handleSubmit={() => navigate("/login")}
+                buttonStyle="small-plain-btn"
+              >
+                Log In
+              </Button>
+              <Button
+                handleSubmit={() => navigate("/register")}
+                buttonStyle="small-bluesix-btn"
+              >
+                Sign Up
+              </Button>
+            </div>
+          )}
+        </nav>
+      </div>
     </header>
   );
 }
