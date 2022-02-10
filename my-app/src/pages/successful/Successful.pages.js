e157133df924cfc94acbbd6b8d51122e3d0bb90f
Aman <amsingh714@gmail.com>
2022-02-10 12:43:58 -0500

    styled successful message page

--- my-app/src/pages/successful/Successful.pages.js
+++ my-app/src/pages/successful/Successful.pages.js
@@ -1,9 +1,9 @@
-import React from "react";
+import "./Successful.styles.scss";
 
 function Successful() {
   return (
     <div className="successful-page-container">
-      <span>Congrats on your purchase!</span>
+      <span>Congratulations on your purchase!</span>
       <span>Your purchases will be emailed to you!</span>
     </div>
   );
