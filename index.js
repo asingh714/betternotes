8529983b1cc65561030fb5c9d553dadc11f0feb7
Aman <amsingh714@gmail.com>
2022-02-15 12:44:41 -0500

    updated middleware for headers

--- index.js
+++ index.js
@@ -21,11 +21,11 @@ server.use(
 
 server.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  // res.header("Access-Control-Allow-Credentials", true);
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  res.header("Access-Control-Allow-Credentials", true);
+  // res.header(
+  //   "Access-Control-Allow-Headers",
+  //   "Origin, X-Requested-With, Content-Type, Accept"
+  // );
   next();
 });
 server.use(morgan("tiny"));
