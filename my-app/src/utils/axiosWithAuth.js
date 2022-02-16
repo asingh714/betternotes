fdbb48ffd0e653b756b9b40b28b09e9935fa4e41
Aman <amsingh714@gmail.com>
2022-02-16 13:11:08 -0500

    updated Access-Control-Allow-Credentials

--- my-app/src/utils/axiosWithAuth.js
+++ my-app/src/utils/axiosWithAuth.js
@@ -6,10 +6,11 @@ export const axiosWithAuth = () => {
     baseURL: "https://better--note.herokuapp.com/api",
     headers: {
       authorization: token,
-      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Origin": "https://betternote.netlify.app",
       "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
       "Access-Control-Allow-Headers":
         "Origin, Content-Type, X-Auth-Token, Authorization, Accept,charset,boundary,Content-Length",
+      "Access-Control-Allow-Credentials": "true",
     },
   });
 };
